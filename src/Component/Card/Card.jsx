5070f8a0a163d8c4fe1d715fb120d3db36289c28
KAHMOOSHA <brhmhelou@gmail.com>
2022-04-19 22:37:17 +0300

    create details page Realtes #4

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -1,8 +1,9 @@
-import { useState,useEffect }from 'react'
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-const url = "https://imdb-api.com/en/API/Top250Movies/k_f5cb9xe5"
+const url = "https://imdb-api.com/en/API/Top250Movies/k_inx1pjtk";
 
-export default  function Card() {
+export default function Card() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -16,17 +17,19 @@ export default  function Card() {
 
   return (
     <div>
-      { movies.map((item,index) => {        
-        return  (
-          <div key={index} >
-            <img src={item.image} alt=""/>
+      {movies.map((item, index) => {
+        return (
+          <div key={index}>
+            <img src={item.image} alt="" />
             <h1>{item.rank}</h1>
             <h2>{item.title}</h2>
             <h2>{item.year}</h2>
+            <Link to="/details">
+              <button onClick={() => changeId(item.id)}>Details</button>
+            </Link>
           </div>
-        )
-      }
-      )}
+        );
+      })}
     </div>
   );
 }
