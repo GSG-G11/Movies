a2f0b303c28ef7af29950b6e55c91e4b841bb1e5
KAHMOOSHA <brhmhelou@gmail.com>
2022-04-20 16:39:57 +0300

    added movie details to new card

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -5,7 +5,7 @@ import image from "../../images/gallery-2.jpg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Card.css";
 
-const url = "https://imdb-api.com/en/API/Top250Movies/k_pbnbr85o";
+const url = "https://imdb-api.com/en/API/Top250Movies/k_inx1pjtk";
 
 export default function Cards({ changeId }) {
   const [movies, setMovies] = useState([]);
@@ -17,54 +17,46 @@ export default function Cards({ changeId }) {
       .then((data) => setMovies(data.items))
       .then(() => setIsLoading(false));
   }, []);
-
+  console.log(movies, 123);
   return (
     <div>
       {isLoading ? (
         <div>Loading...</div>
       ) : (
-        <div>
-          {/* {movies.map((movie, index) => (
-            <div key={index}>
-              <img src={movie.image} alt="" />
-              <h1>{movie.rank}</h1>
-              <h2>{movie.title}</h2>
-              <h2>{movie.year}</h2>
-            <Link to="/details">
-              <button onClick={() => changeId(movie.id)}>Details</button>
-            </Link>
-            </div>
-          
-          ))} */}
-          {/* 
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={movie.image} />
-            <Card.Body>
-              <Card.Title>{movie.title}</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card> */}
-          <Container className="cards__container">
-           
+        <Container className="cards__container">
+          {movies.map((movie, index) => (
             <Card className="card" style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={image} />
+              <Card.Img variant="top" src={movie.image} />
               <Card.Body>
-                <Card.Title className="movie__title">The Batman</Card.Title>
-                <Card.Text className="movie__text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                <Card.Title className="movie__title">{movie.title}</Card.Title>
+                <Card.Text>
+                  <h2>{movie.year}</h2>
                 </Card.Text>
-                <Button variant="primary">Go To Details</Button>
+                <Link to="/details">
+                  <Button onClick={() => changeId} variant="primary">
+                    Go To Details
+                  </Button>
+                </Link>
               </Card.Body>
             </Card>
-     
-            
-          </Container>
-        </div>
+          ))}
+        </Container>
+        // {/* <Container className="cards__container">
+        //   <Card className="card" style={{ width: "18rem" }}>
+        //     <Card.Img variant="top" src={movie.image} />
+        //     <Card.Body>
+        //       <Card.Title className="movie__title">{movie.title}</Card.Title>
+        //       <Card.Text>
+        //       <h2>{movie.year}</h2>
+        //       </Card.Text>
+        //       <Link to="/details">
+        //         <Button onClick={() => changeId} variant="primary">
+        //           Go To Details
+        //         </Button>
+        //       </Link>
+        //     </Card.Body>
+        //   </Card>
+        // </Container> */}
       )}
     </div>
   );
